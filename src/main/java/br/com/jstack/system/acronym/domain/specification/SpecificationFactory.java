f66186ef1e26715ecfd8c5f8604ca3a4b518ef12
Jota <jjjunior@gmail.com>
2025-07-19 16:36:42 -0300

    feat: Add specification pattern implementation with unique name checks

---
+++ src/main/java/br/com/jstack/system/acronym/domain/specification/SpecificationFactory.java
@@ -0,0 +1,21 @@
+package br.com.jstack.system.acronym.domain.specification;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpecificationFactory {
+	
+	public <T> Specification<T> uniqueName(Function<String, Boolean> existsByNameFn,
+	                                       Function<T, String> nameExtractor) {
+		return new UniqueNameSpec<>(existsByNameFn, nameExtractor);
+	}
+	
+	public <T> Specification<T> uniqueNameExcludingSelf(BiFunction<String, Long, Boolean> existsByNameExcludingIdFn,
+	                                                    Function<T, String> nameExtractor,
+	                                                    Function<T, Long> idExtractor) {
+		return new UniqueNameExcludingSelfSpec<>(existsByNameExcludingIdFn, nameExtractor, idExtractor);
+	}
+}
