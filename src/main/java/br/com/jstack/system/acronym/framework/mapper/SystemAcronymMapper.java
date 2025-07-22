870abef960876c3a3fbc8f719221e21a65c4276d
Jota <jjjunior@gmail.com>
2025-07-21 23:14:51 -0300

    feat: simplify SystemAcronym and SystemAcronymMapper by removing unnecessary length constraints and improving mapping logic

--- src/main/java/br/com/jstack/system/acronym/framework/mapper/SystemAcronymMapper.java
+++ src/main/java/br/com/jstack/system/acronym/framework/mapper/SystemAcronymMapper.java
@@ -9,30 +9,21 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface SystemAcronymMapper {
 	
-	@Mapping(target = "type.id", source = "typeId")
+	@Mapping(target = "id", ignore = true)
+	@Mapping(target = "audit", ignore = true)
 	@Mapping(target = "responsibilities", ignore = true)
+	@Mapping(target = "type.id", source = "typeId")
 	@Mapping(target = "domain.id", source = "domainId")
 	@Mapping(target = "devTeam.id", source = "devTeamId")
 	@Mapping(target = "bizTeam.id", source = "bizTeamId")
-	@Mapping(target = "id", ignore = true)
-	@Mapping(target = "audit", ignore = true)
-	@Mapping(target = "name", source = "name")
-	@Mapping(target = "description", source = "description")
-	@Mapping(target = "active", source = "active")
 	SystemAcronym toDomain(SystemAcronymRequest request);
 	
-	
-	@Mapping(target = "typeId", source = "type.id")
-	@Mapping(target = "domainId", source = "domain.id")
-	@Mapping(target = "devTeamId", source = "devTeam.id")
-	@Mapping(target = "bizTeamId", source = "bizTeam.id")
-	@Mapping(target = "id", source = "id")
-	@Mapping(target = "name", source = "name")
-	@Mapping(target = "description", source = "description")
-	@Mapping(target = "active", source = "active")
-	@Mapping(target = "createdBy", source = "audit.createdBy")
-	@Mapping(target = "createdAt", source = "audit.createdAt")
-	@Mapping(target = "updatedBy", source = "audit.updatedBy")
-	@Mapping(target = "updatedAt", source = "audit.updatedAt")
+	@Mapping(target = "systemAcronym.name", source = "name")
+	@Mapping(target = "systemAcronym.description", source = "description")
+	@Mapping(target = "systemAcronym.active", source = "active")
+	@Mapping(target = "systemAcronym.typeId", source = "type.id")
+	@Mapping(target = "systemAcronym.domainId", source = "domain.id")
+	@Mapping(target = "systemAcronym.devTeamId", source = "devTeam.id")
+	@Mapping(target = "systemAcronym.bizTeamId", source = "bizTeam.id")
 	SystemAcronymResponse toResponse(SystemAcronym domain);
 }
